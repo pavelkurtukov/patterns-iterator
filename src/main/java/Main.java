6a298e47ce2dce0674fe2911a825e2e3e5fe352c
pavelkurtukov <pavel@kurtukov.ru>
2023-09-06 22:03:38 +0700

    Решение задачи

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         for (int r : new Randoms(90, 100)) {
--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -3,15 +3,28 @@ import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
+    private final int min;
+    private final int max;
 
     public Randoms(int min, int max) {
-        //...
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
     }
-
-    //...
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true; // Последовательность бесконечная => всегда есть ещё элементы
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min + 1) + min;
+            }
+        };
     }
 }
